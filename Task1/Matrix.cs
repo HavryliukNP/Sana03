73a6b289ffb7b3ed911584d8a4c49e765cf6149c
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-11-29 16:18:44 +0200

    Додав останнє завдання та змінив усе на тип double

--- Task1/Matrix.cs
+++ Task1/Matrix.cs
@@ -8,59 +8,70 @@ namespace Task1
 {
     public class Matrix
     {
-        public static int[] RandomMatrix(int N, int MinValue = -100, int MaxValue = 100)
+        public static double[] RandomMatrix(int N, int MinValue = -100, int MaxValue = 100, 
+            int digitCount = 1)
         {
-            int[] array = new int[N];
+            double[] array = new double[N];
             Random random = new Random();
 
             for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(MinValue, MaxValue);
-            }
+                array[i] = Math.Round(random.Next(MinValue, MaxValue) + 
+                    random.NextDouble(), digitCount);
 
             return array;
         }
-        public static int SumOfNegativeElements(int[] array)
+        public static double SumOfNegativeElements(double[] array, int digitCount = 1)
         {
-            int sum = 0;
+            double sum = 0;
             for (int i = 0; i < array.Length; i++)
-            {
                 if (array[i] < 0)
                     sum += array[i];
-            }
-            return sum;
+            return Math.Round(sum, digitCount);
         }
-        public static int SearchMinElement(int[] array)
+        public static double SearchMinElement(double[] array)
         {
-            int minElement = array[0];
+            double minElement = array[0];
             for (int i = 1; i < array.Length; i++)
-            {
                 if (array[i] < minElement)
                     minElement = array[i];
-            }
             return minElement;
 
         }
-        public static int SearchIndexMaxElement(int[] array)
+        public static int SearchIndexMaxElement(double[] array)
         {
-            int maxElement = array[0];
+            double maxElement = array[0];
             int index = 0;
             for (int i = 1; i < array.Length; i++)
-            {
-                if (array[i] > maxElement)
+                if (array[i] > maxElement) 
+                { 
+                    maxElement = array[i];
                     index = i;
-            }
+                }
             return index;
         }
-        public static int MaxFabsElement(int[] array)
+        public static double MaxFabsElement(double[] array)
         {
-            int maxElement = Math.Fabs
-            int index = 0;
+            double MaxFabsElement = Math.Abs(array[0]);
             for (int i = 1; i < array.Length; i++)
-            {
-                if (array[i] > maxElement)
-                    index = i;
-            }
-            return index;
+                if (Math.Abs(array[i]) > MaxFabsElement)
+                    MaxFabsElement = Math.Abs(array[i]);
+            return MaxFabsElement;
         }
+        public static int SumIndexPositiveElements(double[] array)
+        {
+            int SumIndexMaxElements = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] > 0)
+                    SumIndexMaxElements += i;
+            return SumIndexMaxElements;
+        }
+        public static int SearchCountIntegers(double[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+                if ((array[i] * 10) % 10 == 0)
+                    count++;
+            return count;
+        }
+    }
 }
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,17 +3,26 @@
 Console.Write("Input N elements in array: ");
 int N = int.Parse(Console.ReadLine());
 
-int[] myArray = Matrix.RandomMatrix(N);
+double[] myArray = Matrix.RandomMatrix(N);
 
 Console.WriteLine("Randomly generated array:");
 for (int i = 0; i < myArray.Length; i++)
     Console.WriteLine($"arr[{i}] -> {myArray[i]}");
 
-int sum = Matrix.SumOfNegativeElements(myArray);
+double sum = Matrix.SumOfNegativeElements(myArray);
 Console.WriteLine($"Sum of negative elements = {sum}");
 
-int minElement = Matrix.SearchMinElement(myArray);
+double minElement = Matrix.SearchMinElement(myArray);
 Console.WriteLine($"Minimal element = {minElement}");
 
 int indexMaxElement = Matrix.SearchIndexMaxElement(myArray);
 Console.WriteLine($"Index of maximum element = {indexMaxElement}");
+
+double MaxFabsElement = Matrix.MaxFabsElement(myArray);
+Console.WriteLine($"Maximum element by module = {MaxFabsElement}");
+
+int SumIndex  = Matrix.SumIndexPositiveElements(myArray);
+Console.WriteLine($"Summa indices positive elements = {SumIndex}");
+
+int count = Matrix.SearchCountIntegers(myArray);
+Console.WriteLine($"Number of integers = {count}");
