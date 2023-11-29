76b6b0d249c358b58f2b1a0aa27bf233032c6b96
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-11-29 14:26:29 +0200

    Створив проект та виконав одне з завданнь

---
+++ Task1/Matrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Matrix
+    {
+        public static int[] RandomMatrix(int N, int MinValue = -100, int MaxValue = 100)
+        {
+            int[] array = new int[N];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(MinValue, MaxValue);
+            }
+
+            return array;
+        }
+        public static int SumOfNegativeElements(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                    sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Task1;
+
+Console.Write("Input N elements in array: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] myArray = Matrix.RandomMatrix(N);
+
+Console.WriteLine("Randomly generated array:");
+for (int i = 0; i < myArray.Length; i++)
+    Console.WriteLine($"arr[{i}] -> {myArray[i]}");
+
+int sum = Matrix.SumOfNegativeElements(myArray);
+Console.WriteLine($"Sum of negative elements = {sum}");
